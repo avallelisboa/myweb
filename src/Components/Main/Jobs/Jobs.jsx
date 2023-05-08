7a9dd09fc4399f27ada73ac69986c560dd41b0f7
avallelisboa <48843392+avallelisboa@users.noreply.github.com>
2023-05-08 20:36:03 -0300

    navbutton styles were updated.

--- src/Components/Main/Jobs/Jobs.jsx
+++ src/Components/Main/Jobs/Jobs.jsx
@@ -5,23 +5,18 @@ export const Jobs = () => {
     const [selectedSlide, setSelectedSlide] = useState(0);
 
     const decreaseIndex = ()=>{
-        if(selectedSlide >= 0)
+        if(selectedSlide > 0)
             setSelectedSlide(selectedSlide - 1);
     }
     const increaseIndex = ()=>{
         const totalItems = document.getElementsByClassName("carousel-item").length;
-        if(selectedSlide <= totalItems - 1)
+        if(selectedSlide < totalItems - 1)
             setSelectedSlide(selectedSlide + 1);
     }
     return (
         <>
             <h2>Trabajos</h2>
             <article id="carouselExampleCaptions" className="carousel slide">
-                <div className="carousel-indicators">
-                    <button type="button" onClick={()=>setSelectedSlide(0)} className="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" onClick={()=>setSelectedSlide(1)} aria-label="Slide 2"></button>
-                    <button type="button" onClick={()=>setSelectedSlide(2)} aria-label="Slide 3"></button>
-                </div>
                 <div className="carousel-inner">
                     <figure className={`carousel-item ${selectedSlide == 0 ? 'active' : ''}`}>
                         <img src={IMAGES.faroworld} className="d-block img-fluid w-100" alt="FaroWorld" title="FaroWorld"/>
